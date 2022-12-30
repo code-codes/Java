2bf59b87901095efece33b42b4bcd164fcc58776
code-codes <92877499+code-codes@users.noreply.github.com>
2022-12-30 19:05:23 +0530

    Create Threaddemo.java

---
+++ 30th Nov Exception Handling part 4/Threaddemo.java
@@ -0,0 +1,12 @@
+class MyThread extends Thread {
+}
+public class Threaddemo
+{
+	public static void main(String[] args) {
+	    MyThread t = new MyThread();
+	    t.start();
+	    for(int i = 1; i <= 10; i++) {
+            System.out.println("Main thread");
+        }
+	}
+}
