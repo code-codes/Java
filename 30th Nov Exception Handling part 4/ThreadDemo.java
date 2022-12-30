32d0b487fdcc66aaaff5d8ac070c593ac275664e
Bhavana Boddapati <boddapatibhavana@gmail.com>
2022-12-30 19:08:20 +0530

    Update ThreadDemo.java

--- 30th Nov Exception Handling part 4/ThreadDemo.java
+++ 30th Nov Exception Handling part 4/ThreadDemo.java
@@ -1,12 +1,6 @@
 class MyThread extends Thread {
-    @Override
-    public void run() {
-        for(int i = 1; i <= 10; i++) {
-            System.out.println("child Thread");
-        }
-    }
 }
-public class ThreadDemo
+public class Threaddemo
 {
 	public static void main(String[] args) {
 	    MyThread t = new MyThread();
