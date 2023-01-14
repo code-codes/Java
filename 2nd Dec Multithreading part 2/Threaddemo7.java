5a318732430b4807bc97f8ffe7dfe7eafad8bca9
code-codes <92877499+code-codes@users.noreply.github.com>
2023-01-14 08:06:54 +0530

    Create Threaddemo7.java

---
+++ 2nd Dec Multithreading part 2/Threaddemo7.java
@@ -0,0 +1,18 @@
+class Mythread extends Thread {
+    
+    @Override
+    public void run() {
+        System.out.println("No arg run method");
+    }
+}
+public class Threaddemo7
+{
+	public static void main(String[] args) {
+	    
+	    Mythread r = new Mythread();
+	    
+	    Thread t = new Thread(r);
+	    t.start();
+	    System.out.println("Main thread");
+	}
+}
