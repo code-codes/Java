51ac3c9c8a1c1c78154fa5271e63840cf567c1a5
code-codes <92877499+code-codes@users.noreply.github.com>
2023-01-18 22:18:00 +0530

    Create Threaddemo10.java

---
+++ 5th Dec Multithreading part 3/Threaddemo10.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        Thread.yield();
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Child Thread");
+        }
+    }
+}
+
+
+
+public class Threaddemo10
+{
+	public static void main(String[] args) {
+		
+		//System.out.println("Main Thread priority: " + Thread.currentThread().getPriority());
+		MyThread t = new MyThread();
+		t.start();
+		
+		for(int i = 0; i < 5; i++) {
+            System.out.println("Main Thread");
+        }
+	}
+}
