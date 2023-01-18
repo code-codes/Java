89fa1ef3c4930b5ac5ba3953db4e1045925a5859
Bhavana Boddapati <boddapatibhavana@gmail.com>
2023-01-18 07:34:46 +0530

    created threaddemo9.java

---
+++ 5th Dec Multithreading part 3/Threaddemo9.java
@@ -0,0 +1,28 @@
+//Understanding priority of IllegalThreadStateException
+
+import java.sql.*;
+
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("child Thread priority: " + Thread.currentThread().getPriority());
+    }
+}
+
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Main Thread priority: " + Thread.currentThread().getPriority());
+		MyThread t = new MyThread();
+		t.start();
+	}
+}
+
+
+// OUTPUT:
+// Main Thread priority: 5
+// child Thread priority: 5
