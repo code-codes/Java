6311d56831becc4f78bfba009beeddc22d268469
code-codes <92877499+code-codes@users.noreply.github.com>
2023-01-19 08:26:42 +0530

    Update Threaddemo15.java

--- 6th Dec Multithreading part 4/Threaddemo15.java
+++ 6th Dec Multithreading part 4/Threaddemo15.java
@@ -30,16 +30,24 @@ public class Threaddemo15
 		*/
 		
 		//using lambda expressions:
-		Runnable r = new Runnable() {
+// 		Runnable r = new Runnable() {
+// 		    @Override
+// 		    public void run() {
+//                 for(int i = 1; i <= 5; i++) {
+//                     System.out.println("child thread");
+//                 }
+//             }
+// 		};
+		
+		Thread t = new Thread(new Runnable() {
 		    @Override
 		    public void run() {
                 for(int i = 1; i <= 5; i++) {
                     System.out.println("child thread");
                 }
             }
-		};
-		
-		Thread t = new Thread(r);
+		}
+            );
 		t.start();
 		
 		for(int i = 1; i <= 5; i++) {
