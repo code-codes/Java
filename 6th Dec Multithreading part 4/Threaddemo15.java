140bc949ba46ec4669070ee636709b88fbaac07f
code-codes <92877499+code-codes@users.noreply.github.com>
2023-01-19 08:22:10 +0530

    Create Threaddemo15.java

---
+++ 6th Dec Multithreading part 4/Threaddemo15.java
@@ -0,0 +1,51 @@
+import java.sql.*;
+
+/*
+*functionalInterface
+interface Runnable {
+    void run() {
+        
+    }
+}
+*/
+
+/*
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("child thread");
+        }
+    }
+}
+*/
+
+public class Threaddemo15
+{
+	public static void main(String[] args) {
+	    
+		/*MyRunnable r = new MyRunnable();
+		MyThread t = new MyThread(r);
+		t.start();
+		*/
+		
+		//using lambda expressions:
+		Runnable r = new Runnable() {
+		    @Override
+		    public void run() {
+                for(int i = 1; i <= 5; i++) {
+                    System.out.println("child thread");
+                }
+            }
+		};
+		
+		Thread t = new Thread(r);
+		t.start();
+		
+		for(int i = 1; i <= 5; i++) {
+            System.out.println("Main thread");
+        }
+	}
+}
+
+
