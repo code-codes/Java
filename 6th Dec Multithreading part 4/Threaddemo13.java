b7028feb70c97cb836f97e00ed640de4786c7c87
code-codes <92877499+code-codes@users.noreply.github.com>
2023-01-19 07:46:33 +0530

    Update Threaddemo13.java

--- 6th Dec Multithreading part 4/Threaddemo13.java
+++ 6th Dec Multithreading part 4/Threaddemo13.java
@@ -78,3 +78,52 @@ import java.sql.*;
 // I am sleeping2
 // I am sleeping3
 // I am sleeping4
+
+
+
+//****************************************************************************************
+
+//case 3 - when the child thread enters into waiting/ sleep state after doing his job, interrupt() call have effect
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        for(int i = 0; i < 5; i++) {
+            System.out.println("I am sleeping" + i);
+        }
+        System.out.println("I am now sleeping");
+        try {
+            Thread.sleep(3000);
+        }
+        catch(Exception e) {
+            System.out.print("I got interrupted");
+        }
+            
+    }
+}
+
+
+
+public class Threaddemo13
+{
+	public static void main(String[] args) throws InterruptedException {
+	    
+		
+		MyThread t = new MyThread();
+		t.start();
+		t.interrupt();
+		System.out.println("Main Thread");
+	}
+}
+
+
+
+// output:
+// Main Thread
+// I am sleeping0
+// I am sleeping1
+// I am sleeping2
+// I am sleeping3
+// I am sleeping4
+// I am sleeping
+// I got interrupted
