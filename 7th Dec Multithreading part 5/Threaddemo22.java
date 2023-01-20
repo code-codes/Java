d6ad354ac2b43848ce727e5bbd811b51c8fd3c2c
code-codes <92877499+code-codes@users.noreply.github.com>
2023-01-20 16:28:57 +0530

    Create Threaddemo22.java

---
+++ 7th Dec Multithreading part 5/Threaddemo22.java
@@ -0,0 +1,31 @@
+    
+    int total =0;
+    
+    @Override
+    public void run(){
+        
+        synchronized(this) {
+            System.out.println("Child Thread started calculation");
+        for (int i=0;i<=100 ; i++){
+            total+=i;
+        }
+        System.out.println("child thread giving notification call");
+        this.notify();
+        }
+    }
+}
+
+public class Threaddemo22
+{
+	public static void main(String[] args) throws InterruptedException{
+		ThreadB b=new ThreadB();
+        b.start();
+        Thread.sleep(10000);//10sec
+        synchronized(b) {
+            System.out.println("main method calling wait() method");
+            b.wait();
+            System.out.println("main method got notification call");
+            System.out.println(b.total);
+        }
+	}
+}
