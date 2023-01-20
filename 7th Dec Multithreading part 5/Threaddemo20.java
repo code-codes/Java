6a9acda3eb6306ab53305cffa2d016b537c54342
code-codes <92877499+code-codes@users.noreply.github.com>
2023-01-20 08:02:33 +0530

    Create Threaddemo20.java

---
+++ 7th Dec Multithreading part 5/Threaddemo20.java
@@ -0,0 +1,50 @@
+
+
+class Display   {
+    public void wish(String name){
+        ;;;;;;;;;;;;; //l-lakh lines of code
+        System.out.println("The thread which is getting the lock is: " + Thread.currentThread().getName());
+        synchronized(Display.class){
+            for (int i=1;i<=5;i++ )
+            {
+                System.out.print("Good morning:");
+                try{
+                    Thread.sleep(2000);
+                }
+                catch (InterruptedException e){}
+                    
+                System.out.println(name);
+            }
+        }
+        ;;;;;;;;;;;;;//1-lakh lines of code
+        System.out.println("The thread which is releasing the lock is: " + Thread.currentThread().getName());
+    }
+}
+
+class MyThread extends Thread   {
+    Display d;
+    String name;
+    MyThread(Display d,String name){
+        this.d=d;
+        this.name=name;
+    }
+    public void run(){
+        d.wish(name);
+    }
+}
+
+public class Threaddemo20
+{
+	public static void main(String[] args) {
+		Display d1=new Display();
+        Display d2=new Display();
+        MyThread t1=new MyThread(d1,"dhoni");
+        MyThread t2=new MyThread(d2,"yuvi");
+        
+        t1.setName("dhoni thread");
+        t2.setName("yuvi thread");
+        
+        t1.start();
+        t2.start();
+	}
+}
